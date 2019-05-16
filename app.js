8ff5d1dfb548b02986c0e0f4887f636b1d837010
Reed MacDonald <reedpmacdonald@gmail.com>
2019-05-16 14:55:47 -0700

    messed more with ports and whatnot

--- app.js
+++ app.js
@@ -34,8 +34,10 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+const port = process.env.PORT || 8000;
+
+app.listen(port, function(){
+  console.log("Express server listening on port"+port);
 });
 
 // error handler
